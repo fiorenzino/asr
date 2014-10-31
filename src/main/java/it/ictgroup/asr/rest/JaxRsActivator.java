babff8fa788216a5e5059c670670c0f3933c3f12
fiorenzo pizza <fiorenzino@gmail.com>
2014-10-31 13:08:01 +0100

    rest timer

---
+++ src/main/java/it/ictgroup/asr/rest/JaxRsActivator.java
@@ -0,0 +1,9 @@
+package it.ictgroup.asr.rest;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/rest")
+public class JaxRsActivator extends javax.ws.rs.core.Application
+{
+
+}
---
+++ src/main/java/it/ictgroup/asr/service/rs/TimerRest.java
@@ -0,0 +1,32 @@
+package it.ictgroup.asr.service.rs;
+
+import it.ictgroup.asr.service.FolderService;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/v1/timer")
+@Stateless
+@LocalBean
+public class TimerRest
+{
+
+   @Inject
+   FolderService folderService;
+
+   @GET
+   @Produces("text/plain")
+   @Path("/folderPoller")
+   // @TransactionTimeout(value = 10L, unit = TimeUnit.MINUTES)
+   public String folderPoller() throws Exception
+   {
+      // _01_P7mFolderPollerTimer.executeNow();
+      folderService.verifica();
+      return "OK";
+   }
+
+}
