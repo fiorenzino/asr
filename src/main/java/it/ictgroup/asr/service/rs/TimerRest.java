85c752b1b9a20c591da0af62c94267e922c7e267
fiorenzo pizza <fiorenzino@gmail.com>
2014-10-31 13:10:13 +0100

    <scope>provided</scope>

--- src/main/java/it/ictgroup/asr/service/rs/TimerRest.java
+++ src/main/java/it/ictgroup/asr/service/rs/TimerRest.java
@@ -2,12 +2,16 @@ package it.ictgroup.asr.service.rs;
 
 import it.ictgroup.asr.service.FolderService;
 
+import java.util.concurrent.TimeUnit;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+
+import org.jboss.ejb3.annotation.TransactionTimeout;
 
 @Path("/v1/timer")
 @Stateless
@@ -21,7 +25,7 @@ public class TimerRest
    @GET
    @Produces("text/plain")
    @Path("/folderPoller")
-   // @TransactionTimeout(value = 10L, unit = TimeUnit.MINUTES)
+   @TransactionTimeout(value = 10L, unit = TimeUnit.MINUTES)
    public String folderPoller() throws Exception
    {
       // _01_P7mFolderPollerTimer.executeNow();
