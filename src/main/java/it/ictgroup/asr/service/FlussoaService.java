607427ccfe5419695965200cf233b9d1849be6b0
stefano poli <stefano.poli@ict-group.it>
2014-10-31 15:41:08 +0100

    Errori flussi

--- src/main/java/it/ictgroup/asr/service/FlussoaService.java
+++ src/main/java/it/ictgroup/asr/service/FlussoaService.java
@@ -1,7 +1,9 @@
 package it.ictgroup.asr.service;
 
+import it.coopservice.commons2.domain.Search;
 import it.ictgroup.asr.model.Flussoa1;
 import it.ictgroup.asr.model.Flussoa2;
+import it.ictgroup.asr.model.Flussoc1;
 import it.ictgroup.asr.model.enums.TipologiaFlusso;
 import it.ictgroup.asr.repository.Flussoa1Repository;
 import it.ictgroup.asr.repository.Flussoa2Repository;
@@ -57,7 +59,23 @@ public class FlussoaService implements Serializable
          for (Flussoa1 flussoa1 : righe)
          {
             logger.info(flussoa1);
-            flussoa1Repository.persist(flussoa1);
+            // File A1: archivio contenente le informazioni anagrafiche dell’utente.
+            // File A2: archivio contenente le informazioni sanitarie dell’utente. La segnalazione degli errori deve
+            // essere effettuata in tale archivio anche in caso di errori riscontrati nell’archivio dei dati anagrafici.
+            // Il collegamento delle informazioni contenute nei suddetti archivi è garantito dalla presenza di una
+            // chiave univoca costituita da:
+            // codice regione addebitante + codice ASL/Azienda + codice istituto + numero scheda.
+            // TODO sufficiente per determinare se la riga è già in db???
+            Search<Flussoa1> search = new Search<Flussoa1>(Flussoa1.class);
+            search.getObj().setRegioneAddebitante(flussoa1.getRegioneAddebitante());
+            search.getObj().setAziendaUsl(flussoa1.getAziendaUsl());
+            search.getObj().setCodiceIstitutoDiRicovero(flussoa1.getCodiceIstitutoDiRicovero());
+            search.getObj().setNumeroDellaScheda(flussoa1.getNumeroDellaScheda());
+            List<Flussoa1> resList = flussoa1Repository.getList(search, 0, 0);
+            if(resList != null && resList.size() > 0)
+               flussoa1Repository.update(flussoa1);
+            else
+               flussoa1Repository.persist(flussoa1);
          }
          logger.info(righe.size());
       }
@@ -75,7 +93,23 @@ public class FlussoaService implements Serializable
          for (Flussoa2 flussoa2 : righe)
          {
             logger.info(flussoa2);
-            flussoa2Repository.persist(flussoa2);
+            // File A1: archivio contenente le informazioni anagrafiche dell’utente.
+            // File A2: archivio contenente le informazioni sanitarie dell’utente. La segnalazione degli errori deve
+            // essere effettuata in tale archivio anche in caso di errori riscontrati nell’archivio dei dati anagrafici.
+            // Il collegamento delle informazioni contenute nei suddetti archivi è garantito dalla presenza di una
+            // chiave univoca costituita da:
+            // codice regione addebitante + codice ASL/Azienda + codice istituto + numero scheda.
+            // TODO sufficiente per determinare se la riga è già in db???
+            Search<Flussoa2> search = new Search<Flussoa2>(Flussoa2.class);
+            search.getObj().setRegioneAddebitante(flussoa2.getRegioneAddebitante());
+//            search.getObj().setAziendaUsl(flussoa2.getAziendaUsl());  // TODO dov'è????
+            search.getObj().setCodiceIstituto(flussoa2.getCodiceIstituto());
+            search.getObj().setNumeroDellaScheda(flussoa2.getNumeroDellaScheda());
+            List<Flussoa2> resList = flussoa2Repository.getList(search, 0, 0);
+            if(resList != null && resList.size() > 0)
+               flussoa2Repository.update(flussoa2);
+            else
+               flussoa2Repository.persist(flussoa2);
          }
          logger.info(righe.size());
       }
--- src/main/java/it/ictgroup/asr/service/FlussocService.java
+++ src/main/java/it/ictgroup/asr/service/FlussocService.java
@@ -1,5 +1,6 @@
 package it.ictgroup.asr.service;
 
+import it.coopservice.commons2.domain.Search;
 import it.ictgroup.asr.model.Flussoc1;
 import it.ictgroup.asr.model.Flussoc2;
 import it.ictgroup.asr.model.enums.TipologiaFlusso;
@@ -54,10 +55,26 @@ public class FlussocService implements Serializable
       if (righe != null)
       {
 
-         for (Flussoc1 flussoa1 : righe)
+         for (Flussoc1 flussoc1 : righe)
          {
-            logger.info(flussoa1);
-            flussoc1Repository.persist(flussoa1);
+            logger.info(flussoc1);
+            // File C1: archivio contenente le informazioni anagrafiche dell’utente.
+            // File C2: archivio contenente le informazioni sanitarie dell’utente. La segnalazione degli errori deve
+            // essere effettuata in tale archivio anche in caso di errori riscontrati nell’archivio dei dati anagrafici.
+            // Il collegamento delle informazioni contenute nei suddetti archivi è garantito dalla presenza di una
+            // chiave univoca costituita da:
+            // codice regione addebitante + codice ASL/Azienda + ID + progressivo riga.
+            // TODO sufficiente per determinare se la riga è già in db???
+            Search<Flussoc1> search = new Search<Flussoc1>(Flussoc1.class);
+            search.getObj().setRegioneAddebitante(flussoc1.getRegioneAddebitante());
+            search.getObj().setZonaTerritoriale(flussoc1.getZonaTerritoriale());
+            search.getObj().setId(flussoc1.getId());
+            search.getObj().setProgressivoPeRigaPerRicetta(flussoc1.getProgressivoPeRigaPerRicetta());
+            List<Flussoc1> resList = flussoc1Repository.getList(search, 0, 0);
+            if(resList != null && resList.size() > 0)
+               flussoc1Repository.update(flussoc1);
+            else
+               flussoc1Repository.persist(flussoc1);
          }
          System.out.println(righe.size());
       }
@@ -75,7 +92,23 @@ public class FlussocService implements Serializable
          for (Flussoc2 flussoc2 : righe)
          {
             logger.info(flussoc2);
-            flussoc2Repository.persist(flussoc2);
+            // File C1: archivio contenente le informazioni anagrafiche dell’utente.
+            // File C2: archivio contenente le informazioni sanitarie dell’utente. La segnalazione degli errori deve
+            // essere effettuata in tale archivio anche in caso di errori riscontrati nell’archivio dei dati anagrafici.
+            // Il collegamento delle informazioni contenute nei suddetti archivi è garantito dalla presenza di una
+            // chiave univoca costituita da:
+            // codice regione addebitante + codice ASL/Azienda + ID + progressivo riga.
+            // TODO sufficiente per determinare se la riga è già in db???
+            Search<Flussoc2> search = new Search<Flussoc2>(Flussoc2.class);
+            search.getObj().setRegioneAddebitante(flussoc2.getRegioneAddebitante());
+            search.getObj().setZonaTerritoriale(flussoc2.getZonaTerritoriale());
+            search.getObj().setId(flussoc2.getId());
+            search.getObj().setProgressivoRigaPerRicetta(flussoc2.getProgressivoRigaPerRicetta());
+            List<Flussoc2> resList = flussoc2Repository.getList(search, 0, 0);
+            if(resList != null && resList.size() > 0)
+               flussoc2Repository.update(flussoc2);
+            else
+               flussoc2Repository.persist(flussoc2);
          }
          System.out.println(righe.size());
       }
